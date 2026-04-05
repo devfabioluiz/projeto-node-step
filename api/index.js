7380e967a581ceb0a8ea2af4863940afa22d523a
Fabio Luiz Daemon Da Silva <devfabioluiz@gmail.com>
2026-04-05 19:32:59 -0300

    ajustes vercel2

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ console.log("API index.js loaded, app:", typeof app);
 
 module.exports = app;
 
+
 if (require.main === module) {
   app.listen(port, () => {
     console.log(`Servidor rodando em http://localhost:${port}`);
