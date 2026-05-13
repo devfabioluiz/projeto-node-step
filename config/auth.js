770244f2d31d0fc478db943153fe65e5ea6c2778
unknown <devfabioluiz@gmail.com>
2026-05-13 19:41:19 -0300

    limpando main

--- config/auth.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  tokenSecret: process.env.TOKEN_SECRET || "dev-secret-change-me",
-  tokenExpiresInSeconds: Number(process.env.TOKEN_EXPIRES_IN_SECONDS || 60 * 60 * 8),
-  passwordIterations: 100000,
-  passwordKeyLength: 64,
-  passwordDigest: "sha512",
-};
