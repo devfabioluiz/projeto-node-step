86ae0bc734cdd26475e10a3e4e2169a6b6d7a152
Fabio Luiz Daemon Da Silva <devfabioluiz@gmail.com>
2026-07-01 20:58:24 -0300

    implementa cluster, fork para calculos pesados e benchmark

--- server-single.js
+++ server-single.js
@@ -2,9 +2,9 @@ require("dotenv").config();
 
 const app = require("./src/app");
 
-const conectarBanco = require("./src/database/database");
+// const conectarBanco = require("./src/database/database");
 
-conectarBanco();
+// conectarBanco();
 
 app.listen(process.env.PORT, () => {
   console.log("Servidor rodando em ", process.env.PORT);
